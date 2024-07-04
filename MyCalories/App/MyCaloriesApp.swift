e5267982fc6a8ef57778a3da07cc57978f931a8f
Marcos Bezerra <marcosbezerra@iMac-Pro-de-Marcos.local>
2024-07-03 23:03:15 -0300

    init: Project Structure

---
+++ MyCalories/App/MyCaloriesApp.swift
@@ -0,0 +1,16 @@
+//
+//  MyCaloriesApp.swift
+//  MyCalories
+//
+//  Created by Marcos Bezerra on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
